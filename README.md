910bd29c2d60f5a337a11e8030d56b0a02209c2d
futuristic26 <s.jangir26@gmail.com>
2020-07-25 03:45:05 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# foodshala
-An online food ordering system
+# RestroGirls
+
+A Simple Demo Resturant Management System Project in PHP
+
+
+A pure custom PHP Project. Build for reference!
---
+++ admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About FoodShala</h4>
+      <p>We are FoodShala and we are keen to serve you the best and quality food from different restaurants.</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
